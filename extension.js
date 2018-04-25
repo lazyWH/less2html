618b1579b535ea721904626f1f7fb954205ce755
wanhuan <jack@xian62.com>
2018-04-25 17:01:38 +0800

    separate logic

--- extension.js
+++ extension.js
@@ -21,36 +21,97 @@ function activate(context) {
         // editor.edit(function (edit) {
         //     // itterate through the selections and convert all text to Upper
         //     for (var x = 0; x < sel.length; x++) {
-                
+
         //         edit.replace(sel[x], txt.toUpperCase());
         //     }
         // });        
         let selectionLessText = getSelectionText();
-        let result = convert(selectionLessText);
-        console.log(result);
-        
+        let result;
+        try {
+            result = convert(selectionLessText);
+        } catch (e) {
+            console.log(e);
+        }
+        console.log('result',result);
+
     });
 
     context.subscriptions.push(disposable);
 }
 
-function getSelectionText(){
+
+function convert(selectionLessText) {
+    let finalSourceText = wipeBackdrop(selectionLessText);
+    if (checkBracketsEqual(finalSourceText)) {
+        throw new Error('missing some Brackets');
+    }
+    let convertResult = convertToTree(finalSourceText,'id','root');
+
+    return convertResult;
+}
+function getSelectionText() {
     let editor = vscode.window.activeTextEditor;
     let document = editor.document;
     let sel = editor.selections;
     let txt = document.getText(new vscode.Range(sel[0].start, sel[0].end));
     return txt;
 }
-function convert(selectionLessText){
-    //去除所有注释
-    let textWithoutAnnotation =   selectionLessText.replace(/\/\/.*\n/g,'');
-    //去除所有空格
-    let textWithoutSpace = textWithoutAnnotation.replace(/\s+/g,'');
-    //去除颜色值，和小数，防止干扰
-    let textWithoutBackdrop = textWithoutSpace.replace(/((#\w+)(?=;))|([.]\d+)/,'');
-    // let classOrId = /[#.]\w+(?={})/.exec(textWithoutBackdrop)
-    return textWithoutBackdrop;
+let stack = [];
+function convertToTree(finalSourceText,type,value) {
+    // let regexp = new RegExp(/([.#]\w+(?={))|}/, 'g');
+    // let arr;
+    // let root = {type,value,child:[]};
+    // stack.push(root);
+    // while ((arr = regexp.exec(finalSourceText)) !== null) {
+    //      console.log(`Found ${arr[0]}. Next starts at ${regexp.lastIndex}.`);
+    //     let text = arr[0];
+    //     //如果遇到开标签({)，则入栈这个标签
+    //     if(text!=='}'){
+    //         let node = {type:'id',value:'root',child:[]}
+    //         node.type = getType(text);
+    //         node.value = getValue(text);
+    //         stack.push(node);
+    //     }else{
+    //         let node = stack.pop();
+    //         let parent = stack[stack.length-1];
+    //         parent.child.push(node);
+    //     }
+     
+    // }
+    // console.log('stack',stack);
+    
+
+
+    // return root;
+    return ''
 }
+function getType(text){
+    if(text.indexOf('.')!==-1){
+        return 'class'
+    }
+    return 'id';
+}
+function getValue(text){
+    return text.slice(1,text.length)
+}
+function wipeBackdrop(selectionLessText) {
+    let result = '';
+    //去除所有注释
+    let textWithoutAnnotation = selectionLessText.replace(/\/\/.*\n/g, '');
+    //去除所有空格
+    let textWithoutSpace = textWithoutAnnotation.replace(/\s+/g, '');
+    //去除颜色值，和小数，防止干扰
+    result = textWithoutSpace.replace(/((#\w+)(?=;))|([.]\d+)/g, '');
+    // let classOrId = /[#.]\w+(?={})/.exec(textWithoutBackdrop)
+    return result;
+
+}
+function checkBracketsEqual(finalText) {
+    let leftBrackets = finalText.match(/{/g);
+    let rightBrackets = finalText.match(/}/g);
+    return leftBrackets.length !== rightBrackets.length;
+}
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
